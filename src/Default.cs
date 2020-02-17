f01453428ab054301215d69fdd8955d18fc434c1
Standecco <dlombardo32@gmail.com>
2020-02-17 12:25:04 +0100

    More formatting to MS conventions and project changes

--- src/Default.cs
+++ src/Default.cs
@@ -5,122 +5,121 @@ using UnityEngine;
 
 namespace SolarSailNavigator
 {
+    public class DefaultWindow
+    {
+        // Rectangle object
+        Rect windowPos;
+        int frameID; // Window ID
+        public bool show = false;
+        Navigator navigator;
+        Dictionary<string, string> fields;
 
-	public class DefaultWindow
-	{
-		// Rectangle object
-		Rect windowPos;
-		int frameID; // Window ID
-		public bool show = false;
-		Navigator navigator;
-		Dictionary<string, string> fields;
+        public DefaultWindow (Navigator navigator)
+        {
+            this.navigator = navigator;
+            windowPos = new Rect(705, 100, 0, 0);
+            frameID = GUIUtility.GetControlID(FocusType.Keyboard);
+            fields = new Dictionary<string, string>();
+            fields["Frame"] = navigator.defaultFrame;
+            fields["Angle(0)"] = navigator.defaultAngle0.ToString();
+            fields["Angle(1)"] = navigator.defaultAngle1.ToString();
+            fields["Angle(2)"] = navigator.defaultAngle2.ToString();
+            fields["Days"] = navigator.defaultDays.ToString();
+            fields["Hours"] = navigator.defaultHours.ToString();
+            fields["Throttle"] = navigator.defaultThrottle.ToString();
+            fields["Sail on"] = navigator.defaultSailon.ToString();
+        }
 
-		public DefaultWindow(Navigator navigator)
-		{
-			this.navigator = navigator;
-			windowPos = new Rect(705, 100, 0, 0);
-			frameID = GUIUtility.GetControlID(FocusType.Keyboard);
-			fields = new Dictionary<string, string>();
-			fields["Frame"] = navigator.defaultFrame;
-			fields["Angle(0)"] = navigator.defaultAngle0.ToString();
-			fields["Angle(1)"] = navigator.defaultAngle1.ToString();
-			fields["Angle(2)"] = navigator.defaultAngle2.ToString();
-			fields["Days"] = navigator.defaultDays.ToString();
-			fields["Hours"] = navigator.defaultHours.ToString();
-			fields["Throttle"] = navigator.defaultThrottle.ToString();
-			fields["Sail on"] = navigator.defaultSailon.ToString();
-		}
+        public void DrawWindow ()
+        {
+            if(show)
+            {
+                windowPos = GUILayout.Window(frameID, windowPos, WindowGUI, "Defaults");
+            }
+        }
 
-		public void DrawWindow()
-		{
-			if(show)
-			{
-				windowPos = GUILayout.Window(frameID, windowPos, WindowGUI, "Defaults");
-			}
-		}
+        void WindowGUI (int WindowID)
+        {
+            GUILayout.BeginVertical();
+            foreach(var key in fields.Keys)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(key, GUILayout.Width(80));
+                var newstr = GUILayout.TextField(fields[key], GUILayout.Width(80));
+                float parsedSingle;
+                int parsedInt;
+                bool parsedBool;
+                if(newstr != fields[key])
+                {
+                    fields[key] = newstr;
+                    switch(key)
+                    {
+                        case "Frame":
+                            if(Frame.Frames.ContainsKey(newstr))
+                            {
+                                navigator.defaultFrame = newstr;
+                            }
+                            break;
+                        case "Angle(0)":
+                            if(Single.TryParse(fields[key], out parsedSingle))
+                            {
+                                navigator.defaultAngle0 = Utils.normalizeAngle(parsedSingle);
+                            }
+                            break;
+                        case "Angle(1)":
+                            if(Single.TryParse(fields[key], out parsedSingle))
+                            {
+                                navigator.defaultAngle1 = Utils.normalizeAngle(parsedSingle);
+                            }
+                            break;
+                        case "Angle(2)":
+                            if(Single.TryParse(fields[key], out parsedSingle))
+                            {
+                                navigator.defaultAngle2 = Utils.normalizeAngle(parsedSingle);
+                            }
+                            break;
+                        case "Days":
+                            if(Int32.TryParse(fields[key], out parsedInt))
+                            {
+                                if(parsedInt >= 0)
+                                {
+                                    navigator.defaultDays = (double) parsedInt;
+                                }
+                            }
+                            break;
+                        case "Hours":
+                            if(Int32.TryParse(fields[key], out parsedInt))
+                            {
+                                if(parsedInt >= 0 && parsedInt <= 6)
+                                {
+                                    navigator.defaultHours = (double) parsedInt;
+                                }
+                            }
+                            break;
+                        case "Throttle":
+                            if(Single.TryParse(fields[key], out parsedSingle))
+                            {
+                                if(parsedSingle >= 0 && parsedSingle <= 1)
+                                {
+                                    navigator.defaultThrottle = parsedSingle;
+                                }
+                            }
+                            break;
+                        case "Sail on":
+                            if(Boolean.TryParse(fields[key], out parsedBool))
+                            {
+                                navigator.defaultSailon = parsedBool;
+                            }
+                            break;
+                    }
+                }
 
-		void WindowGUI(int WindowID)
-		{
-			GUILayout.BeginVertical();
-			foreach(var key in fields.Keys)
-			{
-				GUILayout.BeginHorizontal();
-				GUILayout.Label(key, GUILayout.Width(80));
-				var newstr = GUILayout.TextField(fields[key], GUILayout.Width(80));
-				float parsedSingle;
-				int parsedInt;
-				bool parsedBool;
-				if(newstr != fields[key])
-				{
-					fields[key] = newstr;
-					switch(key)
-					{
-						case "Frame":
-							if(Frame.Frames.ContainsKey(newstr))
-							{
-								navigator.defaultFrame = newstr;
-							}
-							break;
-						case "Angle(0)":
-							if(Single.TryParse(fields[key], out parsedSingle))
-							{
-								navigator.defaultAngle0 = Utils.normalizeAngle(parsedSingle);
-							}
-							break;
-						case "Angle(1)":
-							if(Single.TryParse(fields[key], out parsedSingle))
-							{
-								navigator.defaultAngle1 = Utils.normalizeAngle(parsedSingle);
-							}
-							break;
-						case "Angle(2)":
-							if(Single.TryParse(fields[key], out parsedSingle))
-							{
-								navigator.defaultAngle2 = Utils.normalizeAngle(parsedSingle);
-							}
-							break;
-						case "Days":
-							if(Int32.TryParse(fields[key], out parsedInt))
-							{
-								if(parsedInt >= 0)
-								{
-									navigator.defaultDays = (double) parsedInt;
-								}
-							}
-							break;
-						case "Hours":
-							if(Int32.TryParse(fields[key], out parsedInt))
-							{
-								if(parsedInt >= 0 && parsedInt <= 6)
-								{
-									navigator.defaultHours = (double) parsedInt;
-								}
-							}
-							break;
-						case "Throttle":
-							if(Single.TryParse(fields[key], out parsedSingle))
-							{
-								if(parsedSingle >= 0 && parsedSingle <= 1)
-								{
-									navigator.defaultThrottle = parsedSingle;
-								}
-							}
-							break;
-						case "Sail on":
-							if(Boolean.TryParse(fields[key], out parsedBool))
-							{
-								navigator.defaultSailon = parsedBool;
-							}
-							break;
-					}
-				}
-
-				GUILayout.Label(fields[key], GUILayout.Width(80));
-				GUILayout.EndHorizontal();
-			}
-			GUILayout.EndVertical();
-			GUI.DragWindow();
-		}
-	}
+                GUILayout.Label(fields[key], GUILayout.Width(80));
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.EndVertical();
+            GUI.DragWindow();
+        }
+    }
 }
 
--- src/Navigator.cs
+++ src/Navigator.cs
@@ -6,217 +6,215 @@ using PersistentThrust;
 
 namespace SolarSailNavigator
 {
+    public class Navigator : PartModule
+    {
+        // Attitude locked
+        [KSPField(isPersistant = true)]
+        public bool IsLocked = false;
+        // Controls
+        [KSPField(isPersistant = true)]
+        public bool IsControlled = false;
+        [KSPField(isPersistant = true)]
+        public double UT0;
+        // Control parameters by segment
+        [KSPField(isPersistant = true)] // Steering frame
+        public string frames;
+        [KSPField(isPersistant = true)] // Euler angles 0
+        public string angle0s;
+        [KSPField(isPersistant = true)] // Euler angles 1
+        public string angle1s;
+        [KSPField(isPersistant = true)] // Euler angles 2
+        public string angle2s;
+        [KSPField(isPersistant = true)] // Duration in seconds
+        public string durations;
+        [KSPField(isPersistant = true)] // Throttle
+        public string throttles;
+        [KSPField(isPersistant = true)] // Is sail on?
+        public string sailons;
+        // Default control parameters
+        [KSPField(isPersistant = true)] // Steering frame
+        public string defaultFrame = "RTN";
+        [KSPField(isPersistant = true)] // Euler angles 0
+        public float defaultAngle0 = Frame.Frames["RTN"].defaults[0];
+        [KSPField(isPersistant = true)] // Euler angles 1
+        public float defaultAngle1 = Frame.Frames["RTN"].defaults[1];
+        [KSPField(isPersistant = true)] // Euler angles 2
+        public float defaultAngle2 = Frame.Frames["RTN"].defaults[2];
+        [KSPField(isPersistant = true)] // Days
+        public double defaultDays = 10.0;
+        [KSPField(isPersistant = true)] // Hours
+        public double defaultHours = 0.0;
+        [KSPField(isPersistant = true)] // Throttle
+        public float defaultThrottle = 0f;
+        [KSPField(isPersistant = true)] // Is sail on?
+        public bool defaultSailon = true;
+        [KSPField(isPersistant = true)] // Is sail on?
+        public int defaultIWarp = 10;
 
-	public class Navigator : PartModule
-	{
+        // Are there any persistent engines or sails?
+        bool anyPersistent;
 
-		// Attitude locked
-		[KSPField(isPersistant = true)]
-		public bool IsLocked = false;
-		// Controls
-		[KSPField(isPersistant = true)]
-		public bool IsControlled = false;
-		[KSPField(isPersistant = true)]
-		public double UT0;
-		// Control parameters by segment
-		[KSPField(isPersistant = true)] // Steering frame
-		public string frames;
-		[KSPField(isPersistant = true)] // Euler angles 0
-		public string angle0s;
-		[KSPField(isPersistant = true)] // Euler angles 1
-		public string angle1s;
-		[KSPField(isPersistant = true)] // Euler angles 2
-		public string angle2s;
-		[KSPField(isPersistant = true)] // Duration in seconds
-		public string durations;
-		[KSPField(isPersistant = true)] // Throttle
-		public string throttles;
-		[KSPField(isPersistant = true)] // Is sail on?
-		public string sailons;
-		// Default control parameters
-		[KSPField(isPersistant = true)] // Steering frame
-		public string defaultFrame = "RTN";
-		[KSPField(isPersistant = true)] // Euler angles 0
-		public float defaultAngle0 = Frame.Frames["RTN"].defaults[0];
-		[KSPField(isPersistant = true)] // Euler angles 1
-		public float defaultAngle1 = Frame.Frames["RTN"].defaults[1];
-		[KSPField(isPersistant = true)] // Euler angles 2
-		public float defaultAngle2 = Frame.Frames["RTN"].defaults[2];
-		[KSPField(isPersistant = true)] // Days
-		public double defaultDays = 10.0;
-		[KSPField(isPersistant = true)] // Hours
-		public double defaultHours = 0.0;
-		[KSPField(isPersistant = true)] // Throttle
-		public float defaultThrottle = 0f;
-		[KSPField(isPersistant = true)] // Is sail on?
-		public bool defaultSailon = true;
-		[KSPField(isPersistant = true)] // Is sail on?
-		public int defaultIWarp = 10;
+        // Persistent engine controls
+        public Controls controls;
 
-		// Are there any persistent engines or sails?
-		bool anyPersistent;
+        // Engine part modules this controls
+        public List<PersistentEngine> persistentEngines;
 
-		// Persistent engine controls
-		public Controls controls;
+        // Solar sail part modules this controls
+        public List<SolarSailPart> solarSails;
 
-		// Engine part modules this controls
-		public List<PersistentEngine> persistentEngines;
+        /// Show controls
+        [KSPEvent(guiActive = true, guiName = "Show Navigator Controls", active = true)]
+        public void ShowControls ()
+        {
+            IsControlled = true;
+        }
 
-		// Solar sail part modules this controls
-		public List<SolarSailPart> solarSails;
+        /// Hide controls
+        [KSPEvent(guiActive = true, guiName = "Hide Navigator Controls", active = false)]
+        public void HideControls ()
+        {
+            // Remove control window
+            IsControlled = false;
+        }
 
-		// Show controls
-		[KSPEvent(guiActive = true, guiName = "Show Navigator Controls", active = true)]
-		public void ShowControls()
-		{
-			IsControlled = true;
-		}
+        /// When to draw Controls GUI
+        private void OnGUI ()
+        {
+            if(IsControlled & anyPersistent)
+            {
+                controls.DrawControls();
+                controls.defaultWindow.DrawWindow();
+                foreach(var control in controls.controls)
+                {
+                    control.frameWindow.DrawFrameWindow();
+                }
+            }
+        }
 
-		// Hide controls
-		[KSPEvent(guiActive = true, guiName = "Hide Navigator Controls", active = false)]
-		public void HideControls()
-		{
-			// Remove control window
-			IsControlled = false;
-		}
+        /// Initialization
+        public override void OnStart (StartState state)
+        {
 
-		// When to draw Controls GUI
-		private void OnGUI()
-		{
-			if(IsControlled & anyPersistent)
-			{
-				controls.DrawControls();
-				controls.defaultWindow.DrawWindow();
-				foreach(var control in controls.controls)
-				{
-					control.frameWindow.DrawFrameWindow();
-				}
-			}
-		}
+            // Check: are we starting?
+            Debug.Log("[SolarSailNavigator] " + this.part.partName + " OnStart(). IsLocked: " + this.IsLocked);
 
-		// Initialization
-		public override void OnStart(StartState state)
-		{
+            // Base initialization
+            base.OnStart(state);
 
-			// Check: are we starting?
-			Debug.Log("[SolarSailNavigator] " + this.part.partName + " OnStart(). IsLocked: " + this.IsLocked);
+            if(state != StartState.None && state != StartState.Editor)
+            {
 
-			// Base initialization
-			base.OnStart(state);
+                // Find sails and persistent engines
+                foreach(Part p in vessel.parts)
+                {
+                    foreach(PartModule pm in p.Modules)
+                    {
+                        if(pm is SolarSailPart)
+                        {
+                            solarSails.Add((SolarSailPart) pm);
+                        }
+                        else if(pm is PersistentEngine)
+                        {
+                            var pm2 = (PersistentEngine) pm;
+                            if(pm2.IsPersistentEngine)
+                            {
+                                persistentEngines.Add((PersistentEngine) pm);
+                            }
+                        }
+                    }
+                }
 
-			if(state != StartState.None && state != StartState.Editor)
-			{
+                if(solarSails.Count > 0 || persistentEngines.Count > 0)
+                {
+                    // Persistent propulsion found
+                    anyPersistent = true;
 
-				// Find sails and persistent engines
-				foreach(Part p in vessel.parts)
-				{
-					foreach(PartModule pm in p.Modules)
-					{
-						if(pm is SolarSailPart)
-						{
-							solarSails.Add((SolarSailPart) pm);
-						}
-						else if(pm is PersistentEngine)
-						{
-							var pm2 = (PersistentEngine) pm;
-							if(pm2.IsPersistentEngine)
-							{
-								persistentEngines.Add((PersistentEngine) pm);
-							}
-						}
-					}
-				}
+                    // Sail controls
+                    controls = new Controls(this);
+                }
+                else
+                {
+                    anyPersistent = false;
+                    Events["ShowControls"].active = false;
+                    Events["HideControls"].active = false;
+                }
+            }
+        }
 
-				if(solarSails.Count > 0 || persistentEngines.Count > 0)
-				{
-					// Persistent propulsion found
-					anyPersistent = true;
+        /// Updated
+        public override void OnUpdate ()
+        {
 
-					// Sail controls
-					controls = new Controls(this);
-				}
-				else
-				{
-					anyPersistent = false;
-					Events["ShowControls"].active = false;
-					Events["HideControls"].active = false;
-				}
-			}
-		}
+            // Base update
+            base.OnUpdate();
 
-		// Updated
-		public override void OnUpdate()
-		{
+            // Sail deployment GUI
+            if(anyPersistent)
+            {
+                Events["ShowControls"].active = !IsControlled;
+                Events["HideControls"].active = IsControlled;
+            }
+        }
 
-			// Base update
-			base.OnUpdate();
+        /// Physics update
+        public void FixedUpdate ()
+        {
+            if(anyPersistent)
+            {
 
-			// Sail deployment GUI
-			if(anyPersistent)
-			{
-				Events["ShowControls"].active = !IsControlled;
-				Events["HideControls"].active = IsControlled;
-			}
-		}
+                // Universal time
+                double UT = Planetarium.GetUniversalTime();
 
-		// Physics update
-		public void FixedUpdate()
-		{
-			if(anyPersistent)
-			{
+                // Force attitude to specified frame & hold throttle
+                if(FlightGlobals.fetch != null && IsLocked)
+                {
+                    // Set attitude
+                    Control control = controls.Lookup(UT);
+                    vessel.SetRotation(control.frame.qfn(vessel.orbit, UT, control.angles));
+                    // Set throttle
+                    if(isEnabled)
+                    {
+                        // Realtime mode
+                        if(!vessel.packed)
+                        {
+                            vessel.ctrlState.mainThrottle = control.throttle;
+                        }
+                        // Warp mode
+                        else
+                        {
+                            foreach(var pe in persistentEngines)
+                            {
+                                pe.ThrottlePersistent = control.throttle;
+                                pe.ThrustPersistent = control.throttle * pe.engine.maxThrust;
+                                pe.IspPersistent = pe.engine.atmosphereCurve.Evaluate(0);
+                            }
+                        }
+                    }
+                    // Are sails in use?
+                    foreach(var s in solarSails)
+                    {
+                        // Control's "sailon" changes relative to sail's "IsEnabled"
+                        if(control.sailon != s.IsEnabled)
+                        {
+                            if(control.sailon)
+                            { // Sail on
+                                s.DeploySail();
+                            }
+                            else
+                            { // Sail not on
+                                s.RetractSail();
+                            }
+                        }
+                    }
+                }
+            }
 
-				// Universal time
-				double UT = Planetarium.GetUniversalTime();
-
-				// Force attitude to specified frame & hold throttle
-				if(FlightGlobals.fetch != null && IsLocked)
-				{
-					// Set attitude
-					Control control = controls.Lookup(UT);
-					vessel.SetRotation(control.frame.qfn(vessel.orbit, UT, control.angles));
-					// Set throttle
-					if(isEnabled)
-					{
-						// Realtime mode
-						if(!vessel.packed)
-						{
-							vessel.ctrlState.mainThrottle = control.throttle;
-						}
-						// Warp mode
-						else
-						{
-							foreach(var pe in persistentEngines)
-							{
-								pe.ThrottlePersistent = control.throttle;
-								pe.ThrustPersistent = control.throttle * pe.engine.maxThrust;
-								pe.IspPersistent = pe.engine.atmosphereCurve.Evaluate(0);
-							}
-						}
-					}
-					// Are sails in use?
-					foreach(var s in solarSails)
-					{
-						// Control's "sailon" changes relative to sail's "IsEnabled"
-						if(control.sailon != s.IsEnabled)
-						{
-							if(control.sailon)
-							{ // Sail on
-								s.DeploySail();
-							}
-							else
-							{ // Sail not on
-								s.RetractSail();
-							}
-						}
-					}
-				}
-			}
-
-			// Update preview trajectory if it exists
-			if(anyPersistent)
-			{
-				controls.preview.Update(vessel);
-			}
-		}
-	}
+            // Update preview trajectory if it exists
+            if(anyPersistent)
+            {
+                controls.preview.Update(vessel);
+            }
+        }
+    }
 }
